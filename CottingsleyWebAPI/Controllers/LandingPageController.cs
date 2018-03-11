9b61f81fe6e73cd4a996a2f858d5354d90cdaa40
Cottingsley <36730756+Cottingsley@users.noreply.github.com>
2018-03-11 19:39:43 +0530

    Add files via upload

---
+++ CottingsleyWebAPI/Controllers/LandingPageController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CottingsleyModel;
+using CottingsleyService;
+using CottingsleyContracts;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace Cottingsley.Controllers
+{
+    [RoutePrefix("api/LandingPage")]
+    public class LandingPageController : ApiController
+    {
+
+        /// <summary>
+        /// Get only questions
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetAllQuestions")]
+        public IHttpActionResult GetAllOnlyQuestions() {
+            try
+            {
+                ILandingPageSurvey surveyService = new LandingPageSurvey();
+                return Json(new Response
+                {
+                    Message = "Data received successfuly",
+                    Status = true,
+                    Data = surveyService.GetAllQuestions()
+                });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Get Question with options
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetSurveyQuestions")]
+        public IHttpActionResult GetSurveyQuestions() {
+            try
+            {
+                ILandingPageSurvey surveyService = new LandingPageSurvey();
+                return Json(new Response
+                {
+                    Message = "Data received successfuly",
+                    Status = true,
+                    Data = surveyService.GetSurveyQuestionList()
+                });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Get single question by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("GetSurveyQuestionsById")]
+        public IHttpActionResult GetSurveyQuestionsById(int id)
+        {
+            try
+            {
+                ILandingPageSurvey surveyService = new LandingPageSurvey();
+                return Json(new Response
+                {
+                    Message = "Data received successfuly",
+                    Status = true,
+                    Data = surveyService.GetQuestionById(id)
+                });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Post Questions
+        /// </summary>
+        /// <param name="surveyQuestionList"></param>
+        /// <returns></returns>
+        [Route("PostSurveyQuestions")]
+        public IHttpActionResult PostSurveyQuestions(List<SurveyQuestion> surveyQuestionList) {
+            try
+            {
+                ILandingPageSurvey surveyService = new LandingPageSurvey();
+                surveyService.CreateQuestions(surveyQuestionList);
+                return Json(new Response
+                {
+                    Message = "Questions created successfuly",
+                    Status = true,
+                    Data = null
+                });
+            }
+            catch (Exception e) {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Create options of a question
+        /// </summary>
+        /// <param name="questionId"></param>
+        /// <param name="surveyQuestionOptions"></param>
+        /// <returns></returns>
+        [Route("PostSurveyOptions")]
+        public IHttpActionResult PostSurveyOptions(int questionId, List<SurveyQuestionOption> surveyQuestionOptions)
+        {
+            ILandingPageSurvey surveyService = new LandingPageSurvey();
+            surveyService.CreateOptions(questionId, surveyQuestionOptions);
+            return Json(new Response
+            {
+                Message = "Questions created successfuly",
+                Status = true,
+                Data = null
+            });
+        }
+        
+    }
+}
--- CottingsleyWebAPI/Controllers/SchoolController.cs
+++ CottingsleyWebAPI/Controllers/SchoolController.cs
@@ -1,1 +1,108 @@
+﻿using CottingsleyContracts;
+using CottingsleyModel;
+using CottingsleyService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
+namespace Cottingsley.Controllers
+{
+    [RoutePrefix("api/School")]
+    public class SchoolController : ApiController
+    {
+        // GET: api/School
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/School/5
+        
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/School
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        // PUT: api/School/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/School/5
+        public void Delete(int id)
+        {
+        }
+
+        
+        [Route("Create")]
+        [HttpPost]
+        public IHttpActionResult PostSchool([FromBody]School school) {
+            try
+            {
+                ILandingPageSchool schoolService = new LandingPageSchool();
+                schoolService.CreateEntry(school);
+                return Json(new Response
+                {
+                    Message = "School created",
+                    Data = null,
+                    Status = true
+                });
+            }
+            catch (Exception e) {
+                throw e;
+            }
+            
+        }
+
+        [HttpPost]
+        [Route("GetAllSchools")]
+        public IHttpActionResult GetAllSchools() {
+            try
+            {
+                ILandingPageSchool schoolService = new LandingPageSchool();
+                return Json(new Response
+                {
+                    Message = "All school data",
+                    Data = schoolService.GetAll(),
+                    Status = true
+                });
+                
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        [Route("GetSchoolId/id")]
+        public IHttpActionResult GetSchoolById(int id)
+        {
+            try
+            {
+                ILandingPageSchool schoolService = new LandingPageSchool();
+                return Json(new Response
+                {
+                    Message = "School data by id",
+                    Data = schoolService.GetSchoolById(id),
+                    Status = true
+                });
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
